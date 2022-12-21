5145fdbc5e301471ddfa49a5ec4e5fbee807afd2
Cerek Hillen <cerekh@gmail.com>
2022-12-21 21:27:23 +0100

    add future imports

--- pipfreeze2nix/exceptions.py
+++ pipfreeze2nix/exceptions.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class PipFreeze2NixError(Exception):
     pass
 
--- pipfreeze2nix/pep503.py
+++ pipfreeze2nix/pep503.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
 from html.parser import HTMLParser
 from typing import Optional
